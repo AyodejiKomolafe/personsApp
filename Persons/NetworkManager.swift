73d547e20745e9611652303aab829f75b7a44e80
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-26 19:35:00 -0400

    network manager get function done

--- Persons/NetworkManager.swift
+++ Persons/NetworkManager.swift
@@ -17,7 +17,33 @@ final class NetworkManager {
     private init() {}
     
     func getUsers(completed: @escaping (Result<[User],UserError>) -> Void) {
-        
+        guard let url = URL(string: userURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let decoderResponse = try decoder.decode(UserResponse.self, from: data)
+                completed(.success(decoderResponse.data))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
     }
     
     

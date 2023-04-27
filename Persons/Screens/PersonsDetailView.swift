e7e973efae9095aff211fb327ad7cc56f58ee25b
Ayodeji Komolafe <kvngeko@icloud.com>
2023-04-27 18:33:58 -0400

    detail view completed

--- Persons/Screens/PersonsDetailView.swift
+++ Persons/Screens/PersonsDetailView.swift
@@ -30,7 +30,20 @@ struct PersonsDetailView: View {
         .background(Color(.systemBackground))
         .cornerRadius(12)
         .shadow(radius: 40)
-        .overlay(Text("test"), alignment: .topTrailing)
+        .overlay(Button{
+            print("dismiss")
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                Image(systemName: "xmark")
+                    .frame(width: 44, height: 44)
+                    .imageScale(.medium)
+                    .foregroundColor(.black)
+            }
+        }, alignment: .topTrailing)
     }
 }
 
